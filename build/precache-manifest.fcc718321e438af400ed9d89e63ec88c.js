e76afee937a5be1aaff5f83f323ea345561f9121
Brian <boyen@mitake.com.tw>
2019-11-22 12:13:14 +0800

    1.0.1 fix

---
+++ build/precache-manifest.fcc718321e438af400ed9d89e63ec88c.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "2afd9467e2c985595aa3da2aea048d90",
+    "url": "/static/media/frame4.2afd9467.png"
+  },
+  {
+    "revision": "bbdedc6b245834f5061f",
+    "url": "/static/css/main.49df653a.chunk.css"
+  },
+  {
+    "revision": "cdf3e0f95d9ad4404d9a8bb9dd9a6599",
+    "url": "/static/media/hsst.cdf3e0f9.ttf"
+  },
+  {
+    "revision": "ecbcb3f1c9bdfba43dd6",
+    "url": "/static/js/1.ecbcb3f1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "502344d706d2b35bd118c43d13375012",
+    "url": "/static/media/endding.502344d7.jpg"
+  },
+  {
+    "revision": "0509ffd2799b9c42dc2a69d72f497916",
+    "url": "/static/media/afterparty.0509ffd2.jpg"
+  },
+  {
+    "revision": "1f2ad6aff49e48f247c12585919a8c09",
+    "url": "/static/media/club.1f2ad6af.jpg"
+  },
+  {
+    "revision": "b09ca6ad68e6d31ca255098024af032f",
+    "url": "/static/media/gallery.b09ca6ad.jpg"
+  },
+  {
+    "revision": "99bed97f012d6b74bc959a2d04db7c28",
+    "url": "/static/media/venue.99bed97f.jpg"
+  },
+  {
+    "revision": "154e6b1555b08745db30edc6e7cf7ce4",
+    "url": "/static/media/church.154e6b15.jpg"
+  },
+  {
+    "revision": "b4963160f96b946b0c08fcbe5201be95",
+    "url": "/static/media/welcome.b4963160.png"
+  },
+  {
+    "revision": "3a9e821f1ad53fa741df1d0838c248ab",
+    "url": "/static/media/schedule.3a9e821f.png"
+  },
+  {
+    "revision": "8246f0541d778ab27b20f3d8326b2992",
+    "url": "/static/media/gallery.8246f054.png"
+  },
+  {
+    "revision": "5a6c1ecd5455cd6aab57ce5d62492b98",
+    "url": "/static/media/footer.5a6c1ecd.png"
+  },
+  {
+    "revision": "49b1617a420fec027916b07f8243dc39",
+    "url": "/static/media/bkg.49b1617a.png"
+  },
+  {
+    "revision": "a6149cf80c04780413f354a4da60c3c1",
+    "url": "/static/media/church1.a6149cf8.png"
+  },
+  {
+    "revision": "9500479ce397bcb095d748811b524dd7",
+    "url": "/static/media/pic1.9500479c.png"
+  },
+  {
+    "revision": "bbdedc6b245834f5061f",
+    "url": "/static/js/main.bbdedc6b.chunk.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "ecbcb3f1c9bdfba43dd6",
+    "url": "/static/css/1.61ea590b.chunk.css"
+  },
+  {
+    "revision": "e85ea29a3fa3ba67bd83b878a09cdbc9",
+    "url": "/index.html"
+  }
+];
